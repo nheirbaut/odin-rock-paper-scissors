fb2ec34d79348c02ebe20aaae0a8c49e03a86d2d
Niels Heirbaut <niels@opilo.nl>
2024-11-27 15:21:20 +0100

    Play single round

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const HUMAN_WINS = 0;
+const COMPUTER_WINS = 1;
+const TIE = 2;
+
 function numberToGameValue(number) {
     switch (number) {
         case 0:
@@ -21,6 +25,61 @@ function getHumanChoice() {
     return numberToGameValue(choice);
 }
 
+function capitalize(word)
+{
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function getOutcome(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+        return TIE;
+    }
+
+    let humanWins = false;
+
+    switch (humanChoice) {
+        case "rock":
+            humanWins = (computerChoice === "scissors");
+            break;
+        case "paper":
+            humanWins = (computerChoice === "rock");
+            break;
+        case "scissors":
+            humanWins = (computerChoice === "paper");
+            break;
+    }
+
+    return humanWins ? HUMAN_WINS : COMPUTER_WINS;
+}
+
+function getOutcomeMessage(outcome, humanChoice, computerChoice) {
+    switch (outcome) {
+        case TIE:
+            return "No winner! Both chose " + capitalize(humanChoice);
+        case COMPUTER_WINS:
+            return "You lose! " + capitalize(computerChoice) + " defeats " + capitalize(humanChoice);
+        case HUMAN_WINS:
+            return "You win! " + capitalize(humanChoice) + " defeats " + capitalize(computerChoice);
+        default:
+            return "Error! Unknown value for outcome parameter: " + outcome;
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    const humanChoiceLower = humanChoice.toLowerCase();
+    const computerChoiceLower = computerChoice.toLowerCase();
+
+    const outcome = getOutcome(humanChoice, computerChoice);
+    const message = getOutcomeMessage(outcome, humanChoiceLower, computerChoiceLower);
+
+    console.log(message);
+}
+
 let humanScore = 0;
 let computerScore = 0;
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
